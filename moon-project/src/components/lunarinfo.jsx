3d3bff189d3a426e0f04886a06aa72050bb2aa39
frankosgit <132131624+frankosgit@users.noreply.github.com>
2023-09-25 17:30:43 +0200

    started skeleton of page
    Co-authored-by: Alexander Fagerdal <Alexander.Fagerdal@medieinstitutet.se>

---
+++ moon-project/src/components/lunarinfo.jsx
@@ -0,0 +1,14 @@
+import "./lunarinfo.css"
+
+
+const LunarInfo = () => {
+    return (
+    
+        <div className="background">
+            <img src={"./Images/Moon-Cycles.png"} />
+        
+        </div>
+    )
+}
+
+export default LunarInfo
