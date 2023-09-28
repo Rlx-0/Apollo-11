042f8f5984e82fa4408a0195db6382d2c340a6df
frankosgit <132131624+frankosgit@users.noreply.github.com>
2023-09-28 17:33:57 +0200

    written logic for selection of which moon image should be displayed

--- moon-project/src/components/lunarinfo.jsx
+++ moon-project/src/components/lunarinfo.jsx
@@ -1,18 +1,42 @@
 import "./lunarinfo.css"
 
 
-const LunarInfo = (props) => {
+const LunarInfo = ({phaseOfMoon, moonAge, moonRise, darkMode, buttonClickedState}) => {
+
+    /* LOGIC FOR SELECTING WHICH IMG SHOULD BE DISPLAYED ON API DATA
+    
+    const todaysMoonImgSrc = ""
+    if (phaseOfMoon == "Full Moon") {
+        todaysMoonImgSrc == #fullmoonpic#
+    } else if (phaseOfMoon == "Waning Gibbous" {
+        todaysMoonImgSrc == #waninggibbouspic#
+    } else if (phaseOfMoon == "Third Quarter" {
+        todaysMoonImgSrc == #thirdquarterpic#
+    } else if (phaseOfMoon == "Waning Crescent" {
+        todaysMoonImgSrc == #waningcrescentpic#
+    } else if (phaseOfMoon == "New Moon" {
+        todaysMoonImgSrc == #newmoonpic#
+    }else if (phaseOfMoon == "Waxing Crescent" {
+        todaysMoonImgSrc == #waxingCrescentPic#
+    } else if (phaseOfMoon == "First Quarter" {
+        todaysMoonImgSrc == #firstquarterpic#
+    } else if (phaseOfMoon == "Waxing Gibbous" {
+        todaysMoonImgSrc == #waxinggibbouspic#
+    }
+    */
+
+
     return (
         <div>
-            <div className= {props.buttonClickedState ? "landing" : "hide-landing"} >
-                <img  src={props.darkMode ? "./Images/Moon-CyclesV2.png" : "./Images/sun-backgroundV2.jpg"} />
+            <div className= {buttonClickedState ? "landing" : "hide-landing"} >
+                <img  src={darkMode ? "./Images/Moon-CyclesV2.png" : "./Images/sun-backgroundV2.jpg"} />
             </div>
 
             <div>
-                <h2>{props.buttonClickedState ? "button clicked" : "button not clicked"} {props.phaseOfMoon}</h2>
+                <h2>{buttonClickedState ? "button clicked" : "button not clicked"} {phaseOfMoon}</h2>
                 <div>
-                    <h3>{props.moonAge}</h3>
-                    <h3>{props.moonRise}</h3>
+                    <h3>{moonAge}</h3>
+                    <h3>{moonRise}</h3>
                 </div>
             </div>
 
