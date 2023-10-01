9667a99634f25010ac4dd30309b89f7ea35a7947
Alexander Fagerdal <alexanderfagerdal01@gmail.com>
2023-10-01 10:06:58 +0200

    fixed positioning of info page

--- moon-project/src/components/lunarinfo.jsx
+++ moon-project/src/components/lunarinfo.jsx
@@ -9,10 +9,13 @@ const LunarInfo = (props) => {
             </div>
 
             <div>
-                <h2>{props.buttonClickedState ? "button clicked" : "button not clicked"} {props.phaseOfMoon}</h2>
-                <div>
-                    <h3>{props.moonAge}</h3>
-                    <h3>{props.moonRise}</h3>
+                <div className="current-moon">
+                    <img src="./Images/GrayMoon.jpg" alt="" />
+                </div>
+                <h2>{/* {props.buttonClickedState ? "button clicked" : "button not clicked"} */} Moon {props.phaseOfMoon}</h2>
+                <div className="moon-info">
+                    <h3>Age of moon: {props.moonAge}</h3>
+                    <h3>Time of rise: {props.moonRise}</h3>
                 </div>
             </div>
 
